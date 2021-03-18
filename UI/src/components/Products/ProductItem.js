35415a11ec1b15dcdbf9fc6df1eda92e9916fe31
Siddhi Shah <58992453+siddhishah222@users.noreply.github.com>
2021-03-17 21:03:44 -0400

    Add files via upload

---
+++ UI/src/components/Products/ProductItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './ProductItem.css';
+
+const ProductItem = props => {
+  return (
+    <li className="product-item">
+      <h2>{props.name}</h2>
+      <p>Price: ${props.price}</p>
+    </li>
+  );
+};
+
+export default ProductItem;
+
+// output list that will be shown 
